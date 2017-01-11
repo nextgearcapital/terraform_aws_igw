3f11ffaba2dac24753f23b513914308914d62796
Rob Hough <robert.hough@nextgearcapital.com>
2017-01-11 15:10:22 -0500

    Initial add

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "vpc_id" { }
+variable "name" { }
+variable "environment" { }
+variable "team" { }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.igw.id}"
+
+  tags {
+    Name        = "${var.name}-igw"
+    environment = "${var.environment}"
+    team        = "${var.team}"
+  }
+}
+
+output "igw_id" {
+  value = "${aws_internet_gateway.id}"
+}
